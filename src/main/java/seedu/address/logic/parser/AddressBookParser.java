9aac142cd4666d6148a5f552587c15f0cefb6164
sr-magicturtle <neoshaoray@gmail.com>
2026-03-25 12:38:52 +0800

    Implement note add parser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,6 +17,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.NoteAddCommand;
 import seedu.address.logic.commands.TagAddCommand;
 import seedu.address.logic.commands.TagRemoveCommand;
 import seedu.address.logic.commands.ViewCommand;
@@ -87,6 +88,8 @@ public class AddressBookParser {
             return new TagRemoveCommandParser().parse(arguments);
         case ViewCommand.COMMAND_WORD:
             return new ViewCommandParser().parse(arguments);
+        case NoteAddCommand.COMMAND_WORD:
+            return new NoteAddCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
