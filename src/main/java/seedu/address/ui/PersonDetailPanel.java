2018f7494620cadae596b82cfe90669ab3836937
legendgirlie47 <hongtianyu05@gmail.com>
2026-03-26 15:59:57 +0800

    Edit View feature UI

---
+++ src/main/java/seedu/address/ui/PersonDetailPanel.java
@@ -0,0 +1,44 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Person;
+import seedu.address.ui.UiPart;
+
+public class PersonDetailPanel extends UiPart<Region> {
+
+    private static final String FXML = "PersonDetailPanel.fxml";
+
+    @FXML private Label name;
+    @FXML private Label phone;
+    @FXML private Label email;
+    @FXML private Label address;
+
+    @FXML private Label notes;
+    @FXML private Label followUpDate;
+    @FXML private Label circle;
+
+    @FXML private HBox followUpRow;
+    @FXML private HBox notesRow;
+    @FXML private HBox circleRow;
+
+    public PersonDetailPanel(Person person) {
+        super(FXML);
+
+        name.setText(person.getName().fullName);
+        phone.setText(person.getPhone().value);
+        email.setText(person.getEmail().value);
+        address.setText(person.getAddress().value);
+
+        notes.setText(person.getNotes().orElse("-"));
+
+        followUpDate.setText(
+                person.getFollowUpDate().map(d -> d.value.toString()).orElse("-")
+        );
+
+        circle.setText(person.getCircle().orElse("-"));
+    }
+}
